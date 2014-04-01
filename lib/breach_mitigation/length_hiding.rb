33eb1ce217526fe520aad1ff9491d26cf8fc9b36
Dan Dockery <ddockery@socialcast.com>
2014-04-01 16:55:18 -0700

    Use a Rack::Request to detect SSL instead of just using the rack http scheme environment variable

--- lib/breach_mitigation/length_hiding.rb
+++ lib/breach_mitigation/length_hiding.rb
@@ -10,7 +10,7 @@ module BreachMitigation
       status, headers, body = @app.call(env)
 
       # Only pad HTML documents
-      if headers['Content-Type'] =~ /text\/html/ && env['rack.url_scheme'] == 'https'
+      if headers['Content-Type'] =~ /text\/html/ && Rack::Request.new(env).ssl?
         # Copy the existing response to a new object
         response = Rack::Response.new(body, status, headers)
 
